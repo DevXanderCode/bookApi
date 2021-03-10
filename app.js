7f3ad1820f9fcb09110b862150f79c0eec4a0904
Alex Nweke <nwekealexchinedu@gmail.com>
2021-03-10 23:08:02 +0100

    started working on the get for the books endpoint

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+const bookRouter = express.Router();
+
+bookRouter.route('/Books').get((req, res) => {
+  let responseJson = { hello: 'This is my Api' };
+
+  res.json(responseJson);
+});
+
+app.use('/api', bookRouter);
+
 app.get('/', (req, res) => {
   res.send('welcome to my book Api');
 });
