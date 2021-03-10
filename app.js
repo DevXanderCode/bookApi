ffcf653bb20af0d3761dc4df71111f2585e75fc8
Alex Nweke <nwekealexchinedu@gmail.com>
2021-03-10 23:22:13 +0100

    connect mongoose to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const express = require('express');
+const express = require('express'),
+  mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 
 const app = express();
 
