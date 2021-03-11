6b9842c2f3a767cdfaaddb93ef86b6ead21f0cfa
Alex Nweke <nwekealexchinedu@gmail.com>
2021-03-11 11:31:48 +0100

    fixed a typo error

--- app.js
+++ app.js
@@ -22,20 +22,28 @@ const app = express();
 const port = process.env.PORT || 8080;
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 const bookRouter = express.Router();
 
-bookRouter.route('/Books').get((req, res) => {
-  let query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) res.status(500).send(err);
-    else res.json(books);
+bookRouter
+  .route('/Books')
+  .post((req, res) => {
+    let book = new Book(req.body);
+
+    console.log('Logging Book to be added', book);
+    res.send(book);
+  })
+  .get((req, res) => {
+    let query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) res.status(500).send(err);
+      else res.json(books);
+    });
   });
-});
 
 bookRouter.route('/Books/:bookId').get((req, res) => {
   Book.findById(req.params.bookId, (err, book) => {
