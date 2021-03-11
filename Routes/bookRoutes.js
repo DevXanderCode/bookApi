5f7f3bb852bcc18a4736193a70605676712a4e49
Alex Nweke <nwekealexchinedu@gmail.com>
2021-03-12 00:02:38 +0100

    extracted out our routes from App.js

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const routes = () => {
+  const bookRouter = express.Router();
+  const Book = require('../models/bookModel');
+  bookRouter
+    .route('/Books')
+    .post((req, res) => {
+      let book = new Book(req.body);
+      book.save();
+      res.status(201).send(book);
+    })
+    .get((req, res) => {
+      let query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+      Book.find(query, (err, books) => {
+        if (err) res.status(500).send(err);
+        else res.json(books);
+      });
+    });
+
+  bookRouter.route('/Books/:bookId').get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) json.status(500).send(err);
+      else res.json(book);
+    });
+  });
+  return bookRouter;
+};
+
+module.exports = routes;
--- app.js
+++ app.js
@@ -15,40 +15,14 @@ const db = mongoose
     (err) => console.log('Got this error when i tried to connect to mongodb', err)
   );
 
-const Book = require('./models/bookModel');
-
 const app = express();
 
 const port = process.env.PORT || 8080;
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(express.json());
 
-const bookRouter = express.Router();
-
-bookRouter
-  .route('/Books')
-  .post((req, res) => {
-    let book = new Book(req.body);
-    book.save();
-    res.status(201).send(book);
-  })
-  .get((req, res) => {
-    let query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) res.status(500).send(err);
-      else res.json(books);
-    });
-  });
-
-bookRouter.route('/Books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) json.status(500).send(err);
-    else res.json(book);
-  });
-});
+bookRouter = require('./Routes/bookRoutes')();
 
 app.use('/api', bookRouter);
 
