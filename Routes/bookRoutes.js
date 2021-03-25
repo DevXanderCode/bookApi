715a3e4256a8488987375c14a264359260a7c7c6
Alex Nweke <nwekealexchinedu@gmail.com>
2021-03-25 08:01:54 +0100

    finshed working on the patch route for the book endpoint

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -41,8 +41,26 @@ const routes = (Book) => {
       book.genre = req.body.genre;
       book.read = req.body.read;
       book.save();
-      console.log(req.body);
       return res.json(book);
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        delete req.body_id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
   return bookRouter;
 };
