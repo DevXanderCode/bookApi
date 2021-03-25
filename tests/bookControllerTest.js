c0fcc6f33e0dffe5880d7cef1f53dd6297b0b711
Alex Nweke <nwekealexchinedu@gmail.com>
2021-03-25 20:45:43 +0100

    wrote the first book controller test

---
+++ tests/bookControllerTest.js
@@ -0,0 +1,31 @@
+const should = require('should');
+const sinon = require('sinon');
+const bookContoller = require('../controller/bookController');
+
+// mocha follows a BDD kind of style
+describe('Book Controller Tests:', () => {
+  describe('Posts', () => {
+    it('should not allow an empty title on post', () => {
+      const Book = function (book) {
+        this.save = () => {};
+      };
+      const req = {
+        body: {
+          author: 'Nerdy',
+        },
+      };
+
+      const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+        JSON: sinon.spy(),
+      };
+
+      const controller = bookContoller(Book);
+      controller.post(req, res);
+
+      res.status.calledWith(400).should.equal(true, `Bad Status ${res.status.args[0][0]}`);
+      res.send.calledWith('Title is Required').should.equal(true);
+    });
+  });
+});
